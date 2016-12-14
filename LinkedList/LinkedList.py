95cee12eda1adf2feb3de43199c67d452288a6cb
Shubh <shubham.shukla77@hotmail.com>
2016-12-13 19:33:35 -0500

    Added Doubly Linked List Files and updated __doc__ for Linked List Class

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 
 
 class Node :
+    '''Node class holds information stored by each node'''
     def __init__(self, data):
         self.data = data
         self.next = None
 
 
 class LinkedList :
-
+    '''Linked List class stores the linked list and perform a set of operations on the list'''
     def __init__(self):
         self.head = None
 
@@ -97,28 +98,27 @@ class LinkedList :
             print ("Key not Present!")
 
 
+if __name__ == "__main__":
+    list = LinkedList()
 
-list = LinkedList()
+    while True:
+        operation = raw_input("Linked List Operations :\n1.Insert\n2.Search\n3.Delete\n4.Reverse\n5.Print List\n")
+        if operation == "1":
+            newItem = raw_input("Enter the value to be inserted!\n")
+            list.insert(newItem)
+        elif operation == "2":
+            newItem = raw_input("Enter the value to be Searched!\n")
+            list.search(newItem)
+        elif operation == "3":
+            newItem = raw_input("Enter the value to be Deleted!\n")
+            list.delete(newItem)
+        elif operation == "4":
+            list.reverse()
 
-
-while True:
-    operation = raw_input("Linked List Operations :\n1.Insert\n2.Search\n3.Delete\n4.Reverse\n5.Print List\n")
-    if operation == "1":
-        newItem = raw_input("Enter the value to be inserted!\n")
-        list.insert(newItem)
-    elif operation == "2":
-        newItem = raw_input("Enter the value to be Searched!\n")
-        list.search(newItem)
-    elif operation == "3":
-        newItem = raw_input("Enter the value to be Deleted!\n")
-        list.delete(newItem)
-    elif operation == "4":
-        list.reverse()
-
-    elif operation == "5":
-        list.printList()
-    else:
-        "quit()"
+        elif operation == "5":
+            list.printList()
+        else:
+            "quit()"
 
 
 
